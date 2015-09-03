37aa3b6114bdd1ecb7ba0c2927a8761471d24ab9
Nikolai Stoqnov <stoqnovn@yahoo.com>
2015-09-03 12:27:55 +0300

    Модул03, Видео06 - Работа с условни оператори if

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -12,21 +12,31 @@ namespace BoolExplore
 
 			int[] _i = new int[] { 4, 3, 2 };
 
-			int _ui = Convert.ToInt32 (Console.ReadLine () );
+			int _ui = Convert.ToInt32 (Console.ReadLine ());
+//
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0; 
+//
+//			bool _exp01 = _check01 && _check02; //Логическо и &&
+//			bool _exp02 = _check01 || _check02; //Логическо или "||"
+//			bool _exp03 = !_check01;
+//
+//			Console.WriteLine ("Logichesko I:" + _exp01.ToString () );
+//			Console.WriteLine ("Logichesko ILI:" + _exp02.ToString () );
+//			Console.WriteLine ("Otricanie na" + _exp01.ToString () + ": " + _exp03);
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0; 
-
-			bool _exp01 = _check01 && _check02; //Логическо и &&
-			bool _exp02 = _check01 || _check02; //Логическо или "||"
-			bool _exp03 = !_check01;
-
-			Console.WriteLine ("Logichesko I:" + _exp01.ToString () );
-			Console.WriteLine ("Logichesko ILI:" + _exp02.ToString () );
-			Console.WriteLine ("Otricanie na" + _exp01.ToString () + ": " + _exp03);
-
-
-			//Console.WriteLine (_i [ _ui - 1] );
+			if (_ui > 0 && _ui <= _i.Length) {
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				if (_ui > _i.Length)
+				{
+					Console.WriteLine ("Indeksa e prekaleno golqm\n\n\n");		
+				}
+				if (_ui <= 0) 
+				{
+					Console.WriteLine ("Indeksa e prekaleno maluk\n\n\n");	
+				}
+			}
 		}
 	}
 }
