3c7c14e8ed253bdba61b3f3d153a34ca853657dc
Nikolai Stoqnov <stoqnovn@yahoo.com>
2015-09-10 17:46:53 +0300

    Модул04, Видео09 - Цикъл foreach

--- Modul04/ListExplore/Program.cs
+++ Modul04/ListExplore/Program.cs
@@ -43,13 +43,13 @@ namespace ListExplore
 				//Преглед на List
 				if (_userinput.ToLower().Contains ("show") )
 				{
-					Console.Write ( "_list: " );
+					Console.Write ( "Spisuk: " );
 
-					for (int i = 0; i < _list.Count; i++ )
+					foreach (int value in _list)
 					{
-						Console.Write ( _list[i] );
-						if ( i != _list.Count -1 ) Console.Write (", ");
+						Console.Write ( value.ToString () + ", ");
 					}
+//					
 					Console.WriteLine ("\n");
 				}
 				//Размер на List
